de571befe690aea06a35ff3040873909f493c965
Prithvi0 <prithviy96@gmail.com>
2020-03-09 12:46:36 +0530

    Added program for twenty days play to know total amount won or lost

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -7,27 +7,33 @@ WIN=1
 LOSE=0
 
 # VARIABLES
-stake=100							# START WITH A STAKE OF $100
-halfStake=$((stake/2))
-fullStake=$((stake+halfStake))
+days=20
+lost=0
+won=0
 
 # CONDITION TO CHECK IF stake EQUALS TO 50% LESS OR MORE THAN THE INITIAL STAKE
+function TwentyDaysPlay () {
+for((day=1;day<=$days;day++))
+do
+stake=100   #starting stake with $100
+halfStake=$(($stake/2))
+fullStake=$((stake+halfStake))
 while [[ $stake -gt $halfStake && $stake -lt $fullStake ]]
 do
-	gamblingResult=$((RANDOM%2))				# MAKE $1 BET SO EITHER WIN or LOSE $1
+	gamblingResult=$((RANDOM%2))	# MAKE $1 BET SO EITHER WIN or LOSE $1
 	if [[ $gamblingResult -eq $WIN ]]
 	then
 		stake=$((stake+BET))
+		won=$((won+1))
 	elif [[ $gamblingResult -eq $LOSE ]]
 	then
 		stake=$((stake-BET))
+		lost=$((lost+1))
 	fi
 done
-
-# IF WON OR LOST 50% OF THE STAKE, GAMBLER WOULD RESIGN FOR THE DAY
-if [[ $stake -gt $halfStake ]]
-then
-	echo "You Won, resign for the day."
-else
-	echo "You Lost, resign for the day."
-fi
+TotalDaysPlayed="day:$day"
+totalAmountWonLost="totalAmountWon:$won; totalAmountLost:$lost"
+done
+echo "$TotalDaysPlayed; $totalAmountWonLost"
+}
+TwentyDaysPlay	# CALLING THE FUNCTION
