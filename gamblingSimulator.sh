30f31894ba1f4bd8f04088e4e6f96a72be0c6d6c
Prithvi0 <prithviy96@gmail.com>
2020-03-09 11:16:13 +0530

    Added program to resign if gambler won or lost 50% of the stake

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,22 +1,33 @@
 #!/bin/bash -x
 echo "Welcome to Gambling Simulator"
 
-# START WITH A STAKE OF $100 EVERYDAY AND BET OF $1 EVERY GAME.
-
 # CONSTANTS
-STAKE=100
 BET=1
 WIN=1
 LOSE=0
 
-# MAKE $1 BET SO EITHER WIN or LOSE $1
-gamblingResult=$((RANDOM%2))
-if [[ $gamblingResult -eq $WIN ]]
+# VARIABLES
+stake=100							# START WITH A STAKE OF $100
+halfStake=$((stake/2))
+fullStake=$((stake+halfStake))
+
+# CONDITION TO CHECK IF stake EQUALS TO 50% LESS OR MORE THAN THE INITIAL STAKE
+while [[ $stake -gt $halfStake && $stake -lt $fullStake ]]
+do
+	gamblingResult=$((RANDOM%2))				# MAKE $1 BET SO EITHER WIN or LOSE $1
+	if [[ $gamblingResult -eq $WIN ]]
+	then
+		stake=$((stake+BET))
+	elif [[ $gamblingResult -eq $LOSE ]]
+	then
+		stake=$((stake-BET))
+	fi
+done
+
+# IF WON OR LOST 50% OF THE STAKE, GAMBLER WOULD RESIGN FOR THE DAY
+if [[ $stake -gt $halfStake ]]
 then
-	STAKE=$((STAKE+BET))
-	echo "Won"
-elif [[ $gamblingResult -eq $LOSE ]]
-then
-	STAKE=$((STAKE-BET))
-	echo "Lost"
+	echo "You Won, resign for the day."
+else
+	echo "You Lost, resign for the day."
 fi
