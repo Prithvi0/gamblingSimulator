b8fea4905229f0e5973a69207c6a6b45fb580efd
Prithvi0 <prithviy96@gmail.com>
2020-03-09 14:11:33 +0530

    Added program for amount difference of won or lost on monthly basis

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -7,33 +7,37 @@ WIN=1
 LOSE=0
 
 # VARIABLES
-days=20
-lost=0
-won=0
+days=30
 
-# CONDITION TO CHECK IF stake EQUALS TO 50% LESS OR MORE THAN THE INITIAL STAKE
-function TwentyDaysPlay () {
-for((day=1;day<=$days;day++))
-do
-stake=100   #starting stake with $100
-halfStake=$(($stake/2))
-fullStake=$((stake+halfStake))
-while [[ $stake -gt $halfStake && $stake -lt $fullStake ]]
-do
-	gamblingResult=$((RANDOM%2))	# MAKE $1 BET SO EITHER WIN or LOSE $1
-	if [[ $gamblingResult -eq $WIN ]]
-	then
-		stake=$((stake+BET))
-		won=$((won+1))
-	elif [[ $gamblingResult -eq $LOSE ]]
-	then
-		stake=$((stake-BET))
-		lost=$((lost+1))
-	fi
-done
-TotalDaysPlayed="day:$day"
-totalAmountWonLost="totalAmountWon:$won; totalAmountLost:$lost"
-done
-echo "$TotalDaysPlayed; $totalAmountWonLost"
+# FUNCTION TO CHECK AMOUNT WON OR LOST ON MONTHLY BASIS
+function MonthlyLoseWin () {
+	for((day=1;day<=$days;day++))
+	do
+		stake=100   #starting stake with $100
+		halfStake=$(($stake/2))
+		fullStake=$((stake+halfStake))
+		lost=0
+		won=0
+		# CONDITION TO CHECK IF stake EQUALS TO 50% LESS OR MORE THAN THE INITIAL STAKE
+		while [[ $stake -gt $halfStake && $stake -lt $fullStake ]]
+		do
+			gamblingResult=$((RANDOM%2))	# MAKE $1 BET SO EITHER WIN or LOSE $1
+			if [[ $gamblingResult -eq $WIN ]]
+			then
+				stake=$((stake+BET))
+				won=$((won+1))
+			elif [[ $gamblingResult -eq $LOSE ]]
+			then
+				stake=$((stake-BET))
+				lost=$((lost+1))
+			fi
+		done
+		if [[ $won -gt $lost ]]
+		then
+			echo "Day:$day; Won by $won-$lost=$((won-lost))"
+		else
+			echo "Day:$day; Lost by $lost-$won=$((lost-won))"
+		fi
+	done
 }
-TwentyDaysPlay	# CALLING THE FUNCTION
+MonthlyLoseWin	# CALLING THE FUNCTION
